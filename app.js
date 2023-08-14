55d2db0b29539e75f4b4ad1429d6a1144cb4e2e5
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-08-14 11:53:03 +0100

    nc-news-2

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { getTopics } = require('./controllers/topics.controller');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/topics', getTopics);
+
+app.all('*', (req, res) => {
+  res.status(404).send({ msg: 'not found' });
+});
+
+app.use((err, request, response) => {
+  response.status(500).send({ msg: 'Internal server error' });
+});
+
+module.exports = app;
