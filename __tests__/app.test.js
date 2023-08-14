be2e8334d1725e81bee5dc371f65c54f357112ea
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-08-14 12:33:29 +0100

    nc-news-2 update

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,13 +18,28 @@ describe('returns 404 if route not found', () => {
   });
 });
 describe('GET /api/topics', () => {
+  test('GET /api/topics returns list of topics wchich is not empty', () => {
+    return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then((response) => {
+        expect(response.body.topics.length).not.toBe(0);
+      });
+  });
   test('GET /api/topics returns list of topics', () => {
     return request(app)
       .get('/api/topics')
       .expect(200)
       .then((response) => {
-        expect(response.body.topics[0]).toHaveProperty('slug');
-        expect(response.body.topics[0]).toHaveProperty('description');
+        const { topics } = response.body;
+        topics.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
       });
   });
 });
