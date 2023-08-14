d699b8533a7386eb38003c156e93b5e38434acca
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-08-14 15:38:57 +0100

    upgrade to dynamic tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const db = require('../db/connection');
 const seed = require('../db/seeds/seed');
 const data = require('../db/data/test-data/');
+const expectedEndpoints = require('../endpoints.json');
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -45,41 +46,12 @@ describe('GET /api/topics', () => {
 });
 describe('GET /api', () => {
   test('GET /api returns endpoints desctiption object', () => {
-    const expectedEndpoints = {
-      'GET /api': {
-        description:
-          'serves up a json representation of all the available endpoints of the api',
-      },
-      'GET /api/topics': {
-        description: 'serves an array of all topics',
-        queries: [],
-        exampleResponse: {
-          topics: [{ slug: 'football', description: 'Footie!' }],
-        },
-      },
-      'GET /api/articles': {
-        description: 'serves an array of all articles',
-        queries: ['author', 'topic', 'sort_by', 'order'],
-        exampleResponse: {
-          articles: [
-            {
-              title: 'Seafood substitutions are increasing',
-              topic: 'cooking',
-              author: 'weegembump',
-              body: 'Text from the article..',
-              created_at: '2018-05-30T15:59:13.341Z',
-              votes: 0,
-              comment_count: 6,
-            },
-          ],
-        },
-      },
-    };
-    return request(app)
+    request(app)
       .get('/api')
       .expect(200)
       .then((response) => {
-        expect(response.body).toEqual({ endpoints: expectedEndpoints });
+        const endpoindsJSON = JSON.stringify(response.body.endpoints);
+        expect(response.body.endpoints).toStrictEqual(expectedEndpoints);
       });
   });
 });
