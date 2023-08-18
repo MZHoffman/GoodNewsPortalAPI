aa5bbe3e0790a314a54cedba85c28245fcbc2a7f
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-08-18 14:24:42 +0100

    feat\ changing comments 17

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -3,6 +3,7 @@ const {
   selectCommentsForArticle,
   insertCommentForArticle,
   deleteComment,
+  updateComment,
 } = require('../models/comments.models');
 
 exports.getCommentsForArticle = (req, res, next) => {
@@ -44,6 +45,16 @@ exports.removeComment = (req, res, next) => {
       }
       return res.status(204).send();
     })
+    .catch((err) => {
+      return next(err);
+    });
+};
+
+exports.patchComment = (req, res, next) => {
+  const { comment_id } = req.params;
+  const { inc_votes } = req.body;
+  return updateComment(comment_id, inc_votes)
+    .then((comment) => res.status(200).send({ comment }))
     .catch((err) => {
       return next(err);
     });
